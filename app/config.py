4fd02bd2f465729c985ef4dbf647269a99855676
Glory-shine <glorychikoam@gmail.com>
2026-08-01 17:45:27 +0100

    Update DATABASE_URL with new password

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:Lagloire1.0@localhost:5432/academic_service"
+    DATABASE_URL: str = "postgresql://postgres:motdepasse@localhost:5432/academic_service"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
